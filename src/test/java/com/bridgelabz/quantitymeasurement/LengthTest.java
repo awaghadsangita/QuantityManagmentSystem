83964833109f12a68895ccf51d6da46077e46cc0
Upendra Sahu <usahu998@gmail.com>
2019-12-17 19:06:13 +0530

    Added:test for comparing 1 feet with 1 yard,should return false

--- src/test/java/com/bridgelabz/quantitymeasurement/LengthTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/LengthTest.java
@@ -135,4 +135,11 @@ public class LengthTest {
         Assert.assertFalse(result);
     }
 
+    @Test
+    public void given1FeetAnd1Yard_WhenCompared_ShouldReturnFalse() {
+        Length feet = new Length(Length.Unit.FEET, 1.0);
+        Length yard = new Length(Length.Unit.YARD, 1.0);
+        boolean result = feet.compare(yard);
+        Assert.assertFalse(result);
+    }
 }
