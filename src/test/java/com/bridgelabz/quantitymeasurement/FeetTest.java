543b6d102ea4aebf91e0047b648bab7bdd5e36f5
Upendra Sahu <usahu998@gmail.com>
2019-12-17 13:53:23 +0530

    Added: code and test to check equality of Inch object

--- src/test/java/com/bridgelabz/quantitymeasurement/FeetTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/FeetTest.java
@@ -26,4 +26,12 @@ public class FeetTest {
         Inch inch2 = new Inch(0.0);
         Assert.assertEquals(inch1,inch2);
     }
+
+    @Test
+    public void given0InchAnd1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        boolean result=inch1.equals(inch2);
+        Assert.assertFalse(result);
+    }
 }
