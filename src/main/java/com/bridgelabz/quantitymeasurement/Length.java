e695b700e93d3420fddc8cd303ecf1ef7fd46204
Upendra Sahu <usahu998@gmail.com>
2019-12-18 17:38:36 +0530

    Refactor code & test case to handle DRY voilation in UnitConverterEnum

--- src/main/java/com/bridgelabz/quantitymeasurement/Length.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/Length.java
@@ -3,22 +3,18 @@ package com.bridgelabz.quantitymeasurement;
 public class Length {
     private final double value1;
     private final double value2;
-    private final UnitConversionEnum unit;
+    private final UnitConverterEnum unit2;
+    private final UnitConverterEnum unit1;
 
-    public Length(UnitConversionEnum unit, double value1, double value2) {
-        this.unit = unit;
+    public Length(UnitConverterEnum unit1, double value1, UnitConverterEnum unit2, double value2) {
+        this.unit1 = unit1;
+        this.unit2 = unit2;
         this.value1 = value1;
         this.value2 = value2;
     }
 
-    public Length(UnitConversionEnum unit, double value1) {
-        this.unit = unit;
-        this.value1 = value1;
-        this.value2 = 0.0;
-    }
-
     public boolean compare() {
-        return unit.checkConversion(value1, value2);
+        return Double.compare(unit1.convert(value1), unit2.convert(value2)) == 0;
     }
 
     @Override
@@ -28,7 +24,7 @@ public class Length {
         Length length = (Length) o;
         return Double.compare(length.value1, value1) == 0 &&
                 Double.compare(length.value2, value2) == 0 &&
-                unit == length.unit;
+                unit2 == length.unit2 &&
+                unit1 == length.unit1;
     }
-
 }
