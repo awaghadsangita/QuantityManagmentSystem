509e87a72ff9f4b7ea03c0a26e57644c1d27606e
Upendra Sahu <usahu998@gmail.com>
2019-12-19 16:47:40 +0530

    Added:code & test to conversion to temperature

---
+++ src/main/java/com/bridgelabz/quantitymeasurement/TemperatureConverterEnum.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.quantitymeasurement;
+
+public enum TemperatureConverterEnum implements IUnitConversion{
+    CELSIUS {
+        @Override
+        public double convert(double value) {
+            return value;
+        }
+    }, CELSIUS_TO_FAHRENHEIT {
+        @Override
+        public double convert(double value) {
+            return Math.round((value * 9/5) + 32);
+        }
+    }, FAHRENHEIT_TO_CELSIUS {
+        @Override
+        public double convert(double value) {
+            return Math.round((value - 32) * 5/9);
+        }
+    }, FAHRENHEIT{
+        @Override
+        public double convert(double value) {
+            return value;
+        }
+    }
+
+}
